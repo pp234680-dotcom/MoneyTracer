8e58f65d878dc973bbd6d863b9372446860b254e
bow1255 <pp234680@gmail.com>
2026-01-19 01:13:00 +0800

    finish buffer system i  guess

--- MoneyTracer/View/MainView.Designer.cs
+++ MoneyTracer/View/MainView.Designer.cs
@@ -31,24 +31,22 @@ namespace MoneyTracer
         private void InitializeComponent()
         {
             label5 = new Label();
-            panel1 = new Panel();
-            label4 = new Label();
+            spendingNumUpDown = new NumericUpDown();
+            txtBalance = new Label();
             label3 = new Label();
             tabControl1 = new TabControl();
             homepagePage = new TabPage();
-            savingPage = new TabPage();
             panel3 = new Panel();
             txtboxSavingMoney = new Label();
             txtboxSavingName = new Label();
-            spendingPage = new TabPage();
             panel2 = new Panel();
             txtCharge = new Label();
-            txtBankTotal = new Label();
-            label1 = new Label();
-            panel1.SuspendLayout();
+            txtTotalSaving = new Label();
+            savingPage = new TabPage();
+            spendingPage = new TabPage();
+            ((System.ComponentModel.ISupportInitialize)spendingNumUpDown).BeginInit();
             tabControl1.SuspendLayout();
             homepagePage.SuspendLayout();
-            savingPage.SuspendLayout();
             panel3.SuspendLayout();
             panel2.SuspendLayout();
             SuspendLayout();
@@ -56,38 +54,38 @@ namespace MoneyTracer
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(24, 10);
+            label5.Location = new Point(18, 19);
             label5.Name = "label5";
             label5.Size = new Size(152, 19);
             label5.TabIndex = 7;
             label5.Text = "Total Status : Correct";
             // 
-            // panel1
+            // spendingNumUpDown
             // 
-            panel1.BackColor = Color.FromArgb(255, 192, 192);
-            panel1.Controls.Add(label4);
-            panel1.Controls.Add(label3);
-            panel1.Location = new Point(18, 32);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(414, 158);
-            panel1.TabIndex = 4;
+            spendingNumUpDown.Location = new Point(108, 61);
+            spendingNumUpDown.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            spendingNumUpDown.Name = "spendingNumUpDown";
+            spendingNumUpDown.Size = new Size(65, 27);
+            spendingNumUpDown.TabIndex = 3;
+            spendingNumUpDown.ThousandsSeparator = true;
+            spendingNumUpDown.ValueChanged += spendingNumUpDown_ValueChanged;
             // 
-            // label4
+            // txtBalance
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Microsoft JhengHei UI", 15.7310925F, FontStyle.Bold, GraphicsUnit.Point, 136);
-            label4.ForeColor = Color.White;
-            label4.Location = new Point(18, 25);
-            label4.Name = "label4";
-            label4.Size = new Size(194, 33);
-            label4.TabIndex = 2;
-            label4.Text = "Balance : $300";
+            txtBalance.AutoSize = true;
+            txtBalance.Font = new Font("Microsoft JhengHei UI", 15.7310925F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            txtBalance.ForeColor = Color.White;
+            txtBalance.Location = new Point(16, 21);
+            txtBalance.Name = "txtBalance";
+            txtBalance.Size = new Size(216, 33);
+            txtBalance.TabIndex = 2;
+            txtBalance.Text = "Balance : $1,234";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.ForeColor = Color.White;
-            label3.Location = new Point(18, 88);
+            label3.Location = new Point(16, 65);
             label3.Name = "label3";
             label3.Size = new Size(106, 19);
             label3.TabIndex = 1;
@@ -107,26 +105,15 @@ namespace MoneyTracer
             // homepagePage
             // 
             homepagePage.BackColor = Color.FloralWhite;
-            homepagePage.Controls.Add(panel1);
             homepagePage.Controls.Add(label5);
+            homepagePage.Controls.Add(panel3);
+            homepagePage.Controls.Add(panel2);
             homepagePage.Location = new Point(4, 28);
             homepagePage.Name = "homepagePage";
             homepagePage.Padding = new Padding(3);
             homepagePage.Size = new Size(465, 593);
-            homepagePage.TabIndex = 0;
+            homepagePage.TabIndex = 1;
             homepagePage.Text = "Homepage";
-            // 
-            // savingPage
-            // 
-            savingPage.BackColor = Color.FloralWhite;
-            savingPage.Controls.Add(panel3);
-            savingPage.Controls.Add(panel2);
-            savingPage.Location = new Point(4, 28);
-            savingPage.Name = "savingPage";
-            savingPage.Padding = new Padding(3);
-            savingPage.Size = new Size(465, 593);
-            savingPage.TabIndex = 1;
-            savingPage.Text = "Saving";
             // 
             // panel3
             // 
@@ -134,7 +121,7 @@ namespace MoneyTracer
             panel3.BackColor = Color.LightYellow;
             panel3.Controls.Add(txtboxSavingMoney);
             panel3.Controls.Add(txtboxSavingName);
-            panel3.Location = new Point(15, 193);
+            panel3.Location = new Point(15, 230);
             panel3.Name = "panel3";
             panel3.Size = new Size(431, 357);
             panel3.TabIndex = 9;
@@ -159,6 +146,50 @@ namespace MoneyTracer
             txtboxSavingName.TabIndex = 9;
             txtboxSavingName.Text = "Name";
             // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(255, 192, 192);
+            panel2.Controls.Add(spendingNumUpDown);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(txtCharge);
+            panel2.Controls.Add(txtBalance);
+            panel2.Controls.Add(txtTotalSaving);
+            panel2.Location = new Point(15, 45);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(431, 165);
+            panel2.TabIndex = 8;
+            // 
+            // txtCharge
+            // 
+            txtCharge.AutoSize = true;
+            txtCharge.ForeColor = Color.White;
+            txtCharge.Location = new Point(16, 95);
+            txtCharge.Name = "txtCharge";
+            txtCharge.Size = new Size(166, 19);
+            txtCharge.TabIndex = 10;
+            txtCharge.Text = "Buffer Cash Usage : $0";
+            // 
+            // txtTotalSaving
+            // 
+            txtTotalSaving.AutoSize = true;
+            txtTotalSaving.Font = new Font("Microsoft JhengHei UI", 9.07563F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            txtTotalSaving.ForeColor = Color.White;
+            txtTotalSaving.Location = new Point(16, 125);
+            txtTotalSaving.Name = "txtTotalSaving";
+            txtTotalSaving.Size = new Size(133, 19);
+            txtTotalSaving.TabIndex = 2;
+            txtTotalSaving.Text = "Bank Total : $300";
+            // 
+            // savingPage
+            // 
+            savingPage.BackColor = Color.FloralWhite;
+            savingPage.Location = new Point(4, 28);
+            savingPage.Name = "savingPage";
+            savingPage.Padding = new Padding(3);
+            savingPage.Size = new Size(465, 593);
+            savingPage.TabIndex = 0;
+            savingPage.Text = "Saving";
+            // 
             // spendingPage
             // 
             spendingPage.Location = new Point(4, 28);
@@ -169,67 +200,22 @@ namespace MoneyTracer
             spendingPage.Text = "Spending";
             spendingPage.UseVisualStyleBackColor = true;
             // 
-            // panel2
-            // 
-            panel2.BackColor = Color.FromArgb(255, 192, 192);
-            panel2.Controls.Add(txtCharge);
-            panel2.Controls.Add(txtBankTotal);
-            panel2.Controls.Add(label1);
-            panel2.Location = new Point(15, 27);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(431, 123);
-            panel2.TabIndex = 8;
-            // 
-            // txtCharge
-            // 
-            txtCharge.AutoSize = true;
-            txtCharge.ForeColor = Color.White;
-            txtCharge.Location = new Point(171, 72);
-            txtCharge.Name = "txtCharge";
-            txtCharge.Size = new Size(18, 19);
-            txtCharge.TabIndex = 10;
-            txtCharge.Text = "0";
-            // 
-            // txtBankTotal
-            // 
-            txtBankTotal.AutoSize = true;
-            txtBankTotal.Font = new Font("Microsoft JhengHei UI", 15.7310925F, FontStyle.Bold, GraphicsUnit.Point, 136);
-            txtBankTotal.ForeColor = Color.White;
-            txtBankTotal.Location = new Point(17, 20);
-            txtBankTotal.Name = "txtBankTotal";
-            txtBankTotal.Size = new Size(230, 33);
-            txtBankTotal.TabIndex = 2;
-            txtBankTotal.Text = "Bank Total : $300";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Microsoft JhengHei UI", 9.07563F, FontStyle.Bold, GraphicsUnit.Point, 136);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(17, 72);
-            label1.Name = "label1";
-            label1.Size = new Size(161, 19);
-            label1.TabIndex = 2;
-            label1.Text = "Buffer Cash Usage : $";
-            // 
             // MainView
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(507, 649);
+            ClientSize = new Size(505, 649);
             Controls.Add(tabControl1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             Name = "MainView";
             Text = "Money Tracer beta 0.1";
             Load += MainView_Load;
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)spendingNumUpDown).EndInit();
             tabControl1.ResumeLayout(false);
             homepagePage.ResumeLayout(false);
             homepagePage.PerformLayout();
-            savingPage.ResumeLayout(false);
             panel3.ResumeLayout(false);
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
@@ -239,19 +225,18 @@ namespace MoneyTracer
         #endregion
 
         private Label label5;
-        private Panel panel1;
-        private Label label4;
         private Label label3;
         private TabControl tabControl1;
-        private TabPage homepagePage;
         private TabPage savingPage;
+        private TabPage homepagePage;
         private TabPage spendingPage;
         private Panel panel2;
-        private Label txtBankTotal;
-        private Label label1;
+        private Label txtTotalSaving;
         private Label txtboxSavingMoney;
         private Label txtboxSavingName;
         private Panel panel3;
         private Label txtCharge;
+        private NumericUpDown spendingNumUpDown;
+        private Label txtBalance;
     }
 }
--- MoneyTracer/View/MainView.cs
+++ MoneyTracer/View/MainView.cs
@@ -7,14 +7,13 @@ namespace MoneyTracer
     public partial class MainView : Form
     {
         public int savingTotal = 0;
-        public decimal recharge = 0;
-        private decimal rechargeSize1 = 0;
-        private decimal previousVal1 = 0;
-        private decimal nowVal1 = 0;
-        private decimal rechargeSize2 = 0;
-        private decimal previousVal2 = 0;
-        private decimal nowVal2 = 0;
-        private bool isTypeIn = false;
+        public decimal buffer = 0;
+        private decimal previousVal = 0;
+        private decimal nowVal = 0;
+        private int balance = JsonData.BalanceValue;
+        private string titleBalance = "Balance : $";
+        private string titleCharge = "Buffer Cash Usage : $";
+        private string titleTotalSaving = "Total Saving : $";
 
         public MainView()
         {
@@ -23,26 +22,50 @@ namespace MoneyTracer
 
         private string decimalSpreadtor(string val)
         {
-            if (val.Length > 3)
+            if (val[0] != '-')
             {
-                val = val.Insert(val.Length - 3, ",");
-            }
-            else if (val.Length > 6)
-            {
-                val = val.Insert(val.Length - 6, ",");
-                val = val.Insert(val.Length - 3, ",");
+                if (val.Length > 3)
+                {
+                    val = val.Insert(val.Length - 3, ",");
+                }
+                else if (val.Length > 6)
+                {
+                    val = val.Insert(val.Length - 6, ",");
+                    val = val.Insert(val.Length - 3, ",");
+                }
             }
             return val;
+        }
+
+        private void addingNumUpDown(int numUpDownX, ref int numUpDownY, int loopCount, int itemValue)
+        {
+            numUpDownY += 44;
+
+            NumericUpDown numericUpDown = new NumericUpDown();
+            numericUpDown.Name = $"numericUpDown{loopCount}";
+            numericUpDown.Location = new Point(numUpDownX, numUpDownY);
+            numericUpDown.Minimum = 0;
+            numericUpDown.Maximum = 1000000;
+            numericUpDown.Value = itemValue;
+            numericUpDown.Size = new Size(91, 27);
+            numericUpDown.TextAlign = HorizontalAlignment.Right;
+            numericUpDown.ThousandsSeparator = true;
+            numericUpDown.TextChanged += numericUpDown_TextChanged;
+            numericUpDown.GotFocus += numericUpDown_focus;
+            numericUpDown.MouseWheel += numericUpDown_focus;
+            numericUpDown.BackColor = Color.Beige;
+            panel3.Controls.Add(numericUpDown);
         }
 
         private void MainView_Load(object sender, EventArgs e)
         {
             txtboxSavingName.Text = string.Empty;
             txtboxSavingMoney.Text = string.Empty;
-            Dictionary<string, int> savingData = testJsonData.SavingMoneyData;
-            int loopCount = 0;
+            Dictionary<string, int> savingData = JsonData.SavingMoneyData;
+            Dictionary<string, int> weekBudgetData = JsonData.WeekBalanceData;
             int numUpDownX = txtboxSavingMoney.Location.X + 50;
             int numUpDownY = txtboxSavingMoney.Location.Y - 46;
+            int loopCount = 0;
 
             //getting saving data
             foreach (var item in savingData)
@@ -71,55 +94,54 @@ namespace MoneyTracer
                 txtboxSavingName.Size = AddSizeToTheControl(txtboxSavingName.Size);
 
                 //adding nummeric shit
-                numUpDownY += 44;
-                NumericUpDown numericUpDown = new NumericUpDown();
-                numericUpDown.Name = $"numericUpDown{loopCount}";
-                numericUpDown.Location = new Point(numUpDownX, numUpDownY);
-                numericUpDown.Minimum = 0;
-                numericUpDown.Maximum = 1000000;
-                numericUpDown.Value = item.Value;
-                numericUpDown.Size = new Size(91, 27);
-                numericUpDown.TextAlign = HorizontalAlignment.Right;
-                numericUpDown.ThousandsSeparator = true;
-                numericUpDown.TextChanged += numericUpDown_TextChanged;
-                //numericUpDown.ValueChanged += numericUpDown_ValueChanged;
-                numericUpDown.GotFocus += numericUpDown_focus;
-                numericUpDown.MouseWheel += numericUpDown_focus;
-                numericUpDown.BackColor = Color.Beige;
-                panel3.Controls.Add(numericUpDown);
+                addingNumUpDown(numUpDownX, ref numUpDownY, loopCount, item.Value);
             }
 
+            foreach(var item in weekBudgetData)
+            {
+                loopCount++;
+                savingTotal += item.Value;
+
+                //name lenth limit
+                string name = item.Key;
+                if (name.Length > 24)
+                {
+                    name = name.Remove(name.Length - 1, 1);
+                    name += "...";
+                }
+
+                //money comma add
+                string moneyVal = item.Value.ToString();
+                moneyVal = decimalSpreadtor(moneyVal);
+
+                //insert value
+                txtboxSavingName.Text += $"{name}\n\n";
+                txtboxSavingMoney.Text += $"${moneyVal}\n\n";
+
+                //set size
+                txtboxSavingMoney.Size = AddSizeToTheControl(txtboxSavingMoney.Size);
+                txtboxSavingName.Size = AddSizeToTheControl(txtboxSavingName.Size);
+
+                //adding nummeric shit
+                addingNumUpDown(numUpDownX, ref numUpDownY, loopCount, item.Value);
+
+            }
+
+            //get balance value
+            txtBalance.Text = titleBalance + decimalSpreadtor(balance.ToString());
+
             //update total value
-            string savingTotolToString = savingTotal.ToString();
-            savingTotolToString = decimalSpreadtor(savingTotolToString);
-            txtBankTotal.Text = $"Total Saving : ${savingTotolToString}";
+            savingTotal += balance;
+            txtTotalSaving.Text = titleTotalSaving + decimalSpreadtor(savingTotal.ToString());
         }
 
         private static Size AddSizeToTheControl(Size theSize)
         {
             int w = theSize.Width;
             int h = theSize.Height;
-            h += 40;
+            h += 43;
             theSize = new Size(w, h);
             return theSize;
-        }
-        private void numericUpDown_ValueChanged(object sender, EventArgs e)
-        {
-            //if (sender is NumericUpDown a)
-            //{
-            //    previousVal1 = Convert.ToDecimal(a.Text);
-            //    nowVal1 = a.Value;
-            //    rechargeSize1 = nowVal1 - previousVal1;
-            //    recharge -= rechargeSize1;
-            //    txtBankTotal.Focus();
-            //}
-            //if (isTypeIn)
-            //{
-            //    isTypeIn = false;
-            //    return;
-            //}
-            //txtCharge.Text = recharge.ToString();
-
         }
 
         private void numericUpDown_focus(object sender, EventArgs e)
@@ -127,7 +149,7 @@ namespace MoneyTracer
             if (sender is NumericUpDown a)
             {
                 //Get current value 
-                nowVal2 = Convert.ToDecimal(a.Text);
+                nowVal = Convert.ToDecimal(a.Text);
             }
         }
 
@@ -139,25 +161,32 @@ namespace MoneyTracer
                 if (string.IsNullOrEmpty(a.Text)) a.Text = "0";
                 if (a.Text[0] == ',') a.Text = a.Text.Remove(0, 1);
 
-                //check if user use type in method
-                if (Convert.ToDecimal(a.Text) != a.Value) return;
-
                 //make current value as previous value
-                previousVal2 = nowVal2;
-                
+                previousVal = nowVal;
+
                 //update the current value
-                nowVal2 = Convert.ToDecimal(a.Text);
+                nowVal = Convert.ToDecimal(a.Text);
             }
 
-            //tell another method current is type in mode
-            isTypeIn = true;
-
             //subtract each other
-            rechargeSize2 = nowVal2 - previousVal2;
-            recharge -= rechargeSize2;
-            txtCharge.Text = recharge.ToString();
+            buffer -= nowVal - previousVal;
+            txtCharge.Text = titleCharge + decimalSpreadtor(buffer.ToString());
+
+            DoValueUpdate();
         }
 
-        
+        private void spendingNumUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            DoValueUpdate();
+        }
+
+        private void DoValueUpdate()
+        {
+            decimal temp = balance - spendingNumUpDown.Value + buffer;
+            txtBalance.Text = titleBalance + decimalSpreadtor(temp.ToString());
+
+            decimal temp2 = savingTotal - spendingNumUpDown.Value;
+            txtTotalSaving.Text = titleTotalSaving + decimalSpreadtor(temp2.ToString());
+        }
     }
 }
